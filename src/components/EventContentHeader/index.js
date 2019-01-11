674e9dc61fccffe89a292153ef99ed14739f7db1
Cecile <cecilehuynh>
2019-01-11 11:11:46 +0100

    Ajout bouton chevron left et message de eventPage

--- src/components/EventContentHeader/index.js
+++ src/components/EventContentHeader/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './index.scss';
 import Link from 'next/link';
 import Button from '../Button';
+import ChevronLeft from '../../assets/images/chevron-left-solid.svg';
+import SvgButton from '../SvgButton';
 
 const EventContentHeader = () => (
   <div className="event_header">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossOrigin="anonymous" />
     <div className="buttonbox">
       <Link href="/allEventPage">
-        <button type="button" className="backButton"><i className="fas fa-chevron-left" /></button>
+        <SvgButton Svg={ChevronLeft} />
       </Link>
     </div>
     <div id="event_header_left" className="event_header_item">
--- src/components/EventContentParticipant/index.js
+++ src/components/EventContentParticipant/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
 import './index.scss';
+import SvgButton from '../SvgButton';
 import pp from '../../assets/images/mario_profile.jpg';
+import Enveloppe from '../../assets/images/envelope-solid.svg';
 
 const EventContentParticipant = () => (
   <div className="event_participant">
-    <img className="event_participant_item pp" src={pp} alt="profile pic" />
-    <h3 className="event_participant_item">Mario Dupont</h3>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossOrigin="anonymous" />
-    <i className="event_participant_item fa fa-envelope" />
+    <div className="profilePic">
+      <img className="event_participant_item pp" src={pp} alt="profile pic" />
+    </div>
+    <div className="event_participant_item">Mario Dupont</div>
+    <div className="messageIcon">
+      <Link href="/chatPage">
+        <SvgButton Svg={Enveloppe} />
+      </Link>
+    </div>
   </div>
 );
 
