9279b96f10041aa5113df3434a719ac73617f91c
ThibWill <thibault.willer@gmail.com>
2019-01-19 16:26:39 +0100

    map and openid

---
+++ pages/map.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import actions, { actionPropTypes } from '../src/actions';
+import Pin from '../src/components/Pin/index';
+
+class Map extends React.Component {
+  static propTypes = {
+    actions: actionPropTypes.isRequired,
+    events: PropTypes.arrayOf(PropTypes.shape({
+      EVENT_NAME: PropTypes.string.isRequired,
+      ID_EVENT: PropTypes.string.isRequired,
+      LOC_LATITUDE: PropTypes.string.isRequired,
+      LOC_LONGITUDE: PropTypes.string.isRequired,
+    })),
+  };
+
+  static defaultProps = {
+    events: [],
+  }
+
+  componentWillMount = () => {
+    const { actions: { getAllEventsAction } } = this.props;
+    getAllEventsAction({
+      date: null,
+      location: 'Lyon',
+    });
+  }
+
+  get userEvents() {
+    const { events } = this.props;
+    const pins = [];
+    events.forEach((event) => {
+      pins.push(
+        <Pin
+          lat={event.LOC_LATITUDE}
+          lng={event.LOC_LONGITUDE}
+          idEvent={event.ID_EVENT}
+          EVENT_DATE={event.EVENT_DATE}
+          EVENT_NAME={event.EVENT_NAME}
+          LOC_DISTRICT={event.LOC_DISTRICT}
+          MEDIA_CONTENT={event.MEDIA_CONTENT}
+        />,
+      );
+    });
+    return pins;
+  }
+
+  render() {
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDrjmudyF8Tvg9tOemW5GrNsxLCeFB9jXY' }}
+        defaultCenter={{ lat: 45.764043, lng: 4.835659 }}
+        defaultZoom={12}
+        hoverDistance={1}
+      >
+        {this.userEvents}
+      </GoogleMapReact>
+    );
+  }
+}
+
+const mapStateToProps = ({ event: { data: { events } }, location }) => ({ events, location });
+
+export default connect(mapStateToProps, actions)(Map);
