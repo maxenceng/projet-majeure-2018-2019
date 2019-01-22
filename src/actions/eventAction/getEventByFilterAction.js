ef377e11353085d653d74b5c2505d332e467dd3c
ThibWill <thibault.willer@gmail.com>
2019-01-22 20:14:51 +0100

    EventsByName now send good authorizations

--- src/actions/eventAction/getEventByFilterAction.js
+++ src/actions/eventAction/getEventByFilterAction.js
@@ -1,5 +1,7 @@
 import { createAction } from 'redux-actions';
 import axios from '../../helpers/axios';
+import { axiosHeaders, getLocalStorageItem } from '../../helpers/common';
+
 
 export const EVENTS_BY_FILTER_REQUEST = 'EVENTS_BY_FILTER_REQUEST';
 export const EVENTS_BY_FILTER_SUCCESS = 'EVENTS_BY_FILTER_SUCCESS';
@@ -11,7 +13,8 @@ export const eventByFilterError = createAction(EVENTS_BY_FILTER_ERROR);
 
 export default name => (dispatch) => {
   dispatch(eventByFilterRequest());
-  return axios.get(`eventsByName?name=${name}`)
+  const idUser = getLocalStorageItem('idUser');
+  return axios.get(`eventsByName?idUser=${idUser}&name=${name}`, axiosHeaders())
     .then(res => dispatch(eventByFilterSuccess(res)))
     .catch(err => dispatch(eventByFilterError(err)));
 };
